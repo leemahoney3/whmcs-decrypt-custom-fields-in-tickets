6bbd2dc3b9309a117d99c90eb34e215aa30e4adf
Lee Mahoney <lee@leemahoney.dev>
2022-08-17 23:52:03 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# whmcs-decrypt-custom-fields-in-tickets
- 
+# WHMCS Decrypt Custom Fields in Tickets
+
+A hack to show the decrypted version of password custom fields in tickets to the client.
+
+This hook loops through each custom field and only decrypts fields that are of the password type.
+
+To exclude certains fields from the decryption simply add the custom field name to the $excludeFields variable:
+
+```
+$excludeFields = []; // e.g. 'cPanel Password'
+```
+
+## How to install
+
+1. Copy the ```includes``` folder to your root WHMCS directory.
+
+2. Open the viewticket.tpl file in your templates directory and add the following to the end:
+
+```
+{$customfieldjavascript}
+```
+
+## Have a feature request?
+
+Any ideas for it please let me know! I'm happy to implement anything that may benefit the module further. Email all feature requests to lee@leemahoney.dev
+
+## Contributions
+
+Feel free to fork the repo, make changes, then create a pull request!
---
+++ includes/hooks/descryptcustomfieldsintickets.php
@@ -0,0 +1,88 @@
+<?php
+
+use WHMCS\Database\Capsule;
+
+/**
+ * Decrypt Custom Fields in Tickets
+ *
+ * A hack to show the decrypted version of password custom fields in tickets to the client
+ * 
+ * Optionally you can also exclude certain custom field names if you wish.
+ *
+ * @package    WHMCS
+ * @author     Lee Mahoney <lee@leemahoney.dev>
+ * @copyright  Copyright (c) Lee Mahoney 2022
+ * @license    MIT License
+ * @version    1.0.0
+ * @link       https://leemahoney.dev
+ */
+
+
+function decrypt_custom_fields_in_tickets($vars) {
+
+    # Custom Field Names to exclude if needed
+    $excludeFields = [];
+    
+    # Grab the actual ID of the ticket (thanks WHMCS, you'd think a reference to this would be made on a 'view ticket' page)
+    $ticketID = Capsule::table('tbltickets')->where('tid', $_GET['tid'])->first()->id;
+
+    # Start the JavaScript code
+    $script = "<script type='text/javascript'>
+        $(document).ready(function() {
+    ";
+
+    # Loop through each of the custom fields related to the ticket
+    foreach ($vars['customfields'] as $field) {
+
+        # If the field is not a password, it won't be encrypted, abort!
+        if ($field['type'] !== 'password') {
+            continue;
+        }
+
+        # If the custom field is in the exclude list, abort!
+        if (in_array($field['name'], $excludeFields)) {
+            continue;
+        }
+
+        # Grab the actual encrypted data for that field (in the smarty template variables it will just be ****** which is of no good)
+        $value = Capsule::table('tblcustomfieldsvalues')->where(['fieldid' => $field['id'], 'relid' => $ticketID])->first();
+
+        # Only continue if we have a result
+        if (count($value)) {
+
+            # Decrypt the value
+            $request = localAPI('DecryptPassword', ['password2' => $value->value]);
+            
+            # Let's call it a password in this case
+            $password = $request['password'];
+
+            # As we are looping through multiple fields, use JavaScript to alter the value on the page
+            $script .= "
+                $('div[menuitemname=\"{$field['name']}\"]').children('div').eq(1).html('{$password}');
+            ";
+
+        } else {
+
+            # No value returned so let's just display N/A
+            $script .= "
+                $('div[menuitemname=\"{$field['name']}\"]').children('div').eq(1).html('N/A');
+            ";
+
+        }
+
+    }
+
+    # Close the JavaScript
+    $script .= " });
+        </script>
+    ";
+
+    # Pass the JavaScript to a variable that we can use on the template
+    $output['customfieldjavascript'] = $script;
+
+    return $output;
+
+}
+
+# Add the hook
+add_hook('ClientAreaPageViewTicket', 1, 'decrypt_custom_fields_in_tickets');
